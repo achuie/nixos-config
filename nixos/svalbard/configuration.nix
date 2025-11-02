1bd27a44cce09dbc7c04c2167301a9a82d868bb5
achuie <achuie@protonmail.com>
2025-11-02 12:18:11 +0900

    Fix syncthing permissions.

--- nixos/svalbard/configuration.nix
+++ nixos/svalbard/configuration.nix
@@ -206,18 +206,18 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
+    guiAddress = "0.0.0.0:8384";
   };
   users.users.syncthing.extraGroups = [ "users" ];
   # Ensure group read/write permissions
   systemd.services.syncthing.serviceConfig.UMask = "0007";
   # Syncthing needs access to user's home
   systemd.tmpfiles.rules = [
-    "d /home/achuie 0750 achuie syncthing"
+    "d /home/achuie 0751 achuie syncthing"
     "d /home/achuie/Sync 2770 achuie syncthing"
   ];
   systemd.services.fix-syncthing-perms = {
     serviceConfig.Type = "oneshot";
-    serviceConfig.User = "achuie";
     script = ''
       find "Sync" -type f \( ! -group syncthing -or ! -perm -g=rw \) -not -path "*/.st*" -exec chgrp syncthing {} \; -exec chmod g+rw {} \;
       find "Sync" -type d \( ! -group syncthing -or ! -perm -g=rwxs \) -not -path "*/.st*" -exec chgrp syncthing {} \; -exec chmod g+rwxs {} \;
